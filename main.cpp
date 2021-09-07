1b6227629f2049cafe3793ecef3e31e2db9460e7
Яновец Сергей <sdy@nita.ru>
2021-09-08 00:35:34 +0300

    ExpandStirng - test

--- main.cpp
+++ main.cpp
@@ -7,5 +7,17 @@ int main(int argc, char* argv[])
     plug_key::CModeInfoPlug lib;
     bool bRes = lib.Load();
     std::cout << "RES>" << bRes << std::endl;
+    std::string sText =
+         "%%NITAROOT%%"
+         "/[GROUP]"
+         "/[CFG]"
+         "/[PRODUCT]"
+         "/[MODE]"
+         "/[LANG]"
+         "/[COMPUTER]"
+         "/[UID]";
+    lib.ExpandString( sText );
+    std::cout << sText << std::endl;
+    lib.Free();
     return 0;
 }
