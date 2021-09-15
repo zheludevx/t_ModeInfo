eb27230de930736aa75e803362fee081a1852c07
Желудев Александр <zheludevx@gmail.com>
2021-09-15 21:15:01 +0300

    Last work

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <stdlib.h>
 #include <cstdio>
+#include <exception>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/fstream.hpp>
 #include <iostream>
@@ -38,50 +39,28 @@ int main(int argc, char* argv[])
     }
     std::string str;
     str = getenv("NITAETC");
-    boost::filesystem::directory_iterator begin(str), end;
+    boost::filesystem::directory_iterator begin(str), end, begin1(str);
     std::cout << std::endl << "$NITAETC groups:" << std::endl;
-    for ( ; begin != end; ++begin)
+    try
     {
-        if (begin->path().extension() == ".xml")
+        for ( ; begin != end; ++begin)
         {
-            if (argc == 1)
-            {
-                if(argv[0] == "system-msk.xml")
-                {
-                    std::cout << "Set \"system-msk.xml\" as system.xml? (y/n)" << std::endl;
-                    std::string x;
-                    if (x == "y")
-                    {
-                        try
-                        {
-                            boost::filesystem::path from =
-                                    complete(boost::filesystem::path("system-msk.xml", boost::filesystem::native));
-                            boost::filesystem::path here =
-                                    complete(boost::filesystem::path("system.xml", boost::filesystem::native));
-                            copy_file(from, here);
-                        }
-                        catch (std::exception& e)
-                        {
-                            std::cerr <<e.what() << std::endl;
-                        }
-                        char oldfilename[] = "system.xml";
-                        char newfilename[] = "system.xml.bak";
-                        if ( rename(oldfilename, newfilename) == 0)
-                            continue;
-                        else
-                            std::cout << "rename error" << std::endl;
-                    }
-
-                    if (x == "n")
-                        std::cout << "Operation canceled." << std::endl;
-                }
-            }
-            else
-                std::cout << "ERR> Wrong command line" << std::endl;
-
-            std::cout << *begin << std::endl;
+            if (begin->path().extension() == ".xml")
+                std::cout << *begin << std::endl;
+        }
+        std::cout << std::endl;
+        for ( ; begin1 != end; ++begin1)
+        {
+            if ((begin1->path().string().find("system") == 19) && (begin1->path().string().find(".xml") == 29 ))
+                std::cout << begin1->path() << std::endl;
         }
     }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what();
+        return 1;
+    }
+    std::cout << std::endl << std::endl;
     lib.Free();
     return 0;
 }
