c8502c332766a7b3c33082703454f1fe8c8ab143
Желудев Александр <zheludevx@gmail.com>
2021-09-22 19:55:18 +0300

    Last work

--- main.cpp
+++ main.cpp
@@ -11,74 +11,62 @@
 
 bool checkSystem (std::string &s)
 {
+    bool bRes = false;
     unsigned int system = s.find("system");
     std::cout << "Подстрока найдена в позиции "<< system << std::endl;
     if (system == 0)
     {
         std::cout << "Данная подстрока подходит" << std::endl << std::endl;
-        bool bRes = true;
-        return bRes;
+        bRes = true;
     }
     else
-    {
         std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
-        bool bRes = false;
-        return bRes;
-    }
+    return bRes;
 }
 
 bool checkXml (std::string &s)
 {
+    bool bRes = false;
     unsigned int xml = s.find(".xml");
     std::cout << "Индекс позиции расширения "<< xml << std::endl;
     if ((xml + 4) == s.length())
     {
         std::cout << "Данная подстрока подходит" << std::endl << std::endl;
-        bool bRes = true;
-        return bRes;
+        bRes = true;
     }
     else
-    {
         std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
-        bool bRes = false;
-        return bRes;
-    }
+    return bRes;
 }
 
 bool checkRxml(std::string &s)
 {
+    bool bRes = false;
     unsigned int rxml = s.find(".xml");
     std::cout << "Индекс r позиции расширения "<< rxml << std::endl;
     if (rxml  == (s.length() - 4 ))
     {
         std::cout << "Данная подстрока подходит" << std::endl << std::endl;
-        bool bRes = true;
-        return bRes;
+        bRes = true;
     }
     else
-    {
         std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
-        bool bRes = false;
-        return bRes;
-    }
+    return bRes;
 }
 
 bool sVivod (std::string &s)
 {
+    bool bRes = false;
     std::string sSubstr = s.substr(s.length() - 4);
     std::cout << "Подстрока из последних 4 символов: " << "\""<< sSubstr << "\"" << std::endl;
     if (sSubstr == ".xml")
     {
         std::cout << "Данная подстрока подходит" << std::endl << std::endl;
-        bool bRes = true;
-        return bRes;
+        bRes = true;
     }
     else
-    {
         std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
-        bool bRes = false;
-        return bRes;
-    }
+    return bRes;
 }
 
 int main(int argc, char* argv[])
@@ -111,12 +99,27 @@ int main(int argc, char* argv[])
         std::cout<< "\"" << arr[i] << "\"" << std::endl;
     }
     std::cout << std::endl << std::endl;
-    std::string s;
-    getline(std::cin, s);
-    checkSystem (s);
-    checkXml (s);
-    checkRxml(s);
-    sVivod (s);
+    std::string str = getenv("NITAETC");
+    std::vector<std::string> s;
+    boost::filesystem::directory_iterator begin(str), end;
+    try
+    {
+        for ( ; begin != end; ++begin)
+            s.push_back(begin->path().filename());
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what();
+        return 1;
+    }
+    for (unsigned int i = 0; i < s.size(); i++)
+    {
+        std::cout << s[i] << std::endl;
+        checkSystem (s[i]);
+        checkXml (s[i]);
+        checkRxml (s[i]);
+        sVivod (s[i]);
+    }
     lib.Free();
     return 0;
 }
