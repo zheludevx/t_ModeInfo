1e0ea0e45d8662f335e1c5b9854cdca6f156d4d5
Желудев Александр <zheludevx@gmail.com>
2021-09-26 13:36:20 +0300

    Last work

--- main.cpp
+++ main.cpp
@@ -9,87 +9,27 @@
 #include <boost/filesystem/path.hpp>
 #include <iostream>
 
-bool checkSystem (std::string &vStr)
+bool checkSystem (const std::string& sFileName)
 {
     bool bRes = false;
-    size_t sztSystem = vStr.find("system");
+    size_t sztSystem = sFileName.find("system");
     if (sztSystem != std::string::npos)
     {
-        std::cout << "Подстрока найдена в позиции "<< sztSystem << std::endl;
         if (sztSystem == 0)
-        {
-            std::cout << "Данная подстрока подходит" << std::endl << std::endl;
             bRes = true;
-        }
-        else
-            std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
-        return bRes;
     }
-    else
-    { 
-        std::cout << "Данная подстрока не найдена" << std::endl << std::endl;
-        return bRes;
-    }   
+    return bRes;
 }
 
-bool checkXml (std::string &vStr)
+bool checkXml (const std::string& sFileName)
 {
     bool bRes = false;
-    size_t sztXml = vStr.find(".xml");
+    size_t sztXml = sFileName.find(".xml");
     if (sztXml != std::string::npos)
     {
-        std::cout << "Индекс позиции расширения "<< sztXml << std::endl;
-        if ((sztXml + 4) == vStr.length())
-        {
-            std::cout << "Данная подстрока подходит" << std::endl << std::endl;
+        if ((sztXml + 4) == sFileName.length())
             bRes = true;
-        }
-        else
-            std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
-        return bRes;
     }
-    else
-    {
-        std::cout << "Данное расширение не найдено" << std::endl << std::endl;
-        return bRes;
-    }
-}
-
-bool checkRxml(std::string &vStr)
-{
-    bool bRes = false;
-    size_t sztRxml = vStr.find(".xml");
-    if (sztRxml != std::string::npos)
-    {
-        std::cout << "Индекс r позиции расширения "<< sztRxml << std::endl;
-        if (sztRxml  == (vStr.length() - 4 ))
-        {
-            std::cout << "Данная подстрока подходит" << std::endl << std::endl;
-            bRes = true;
-        }
-        else
-            std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
-        return bRes;
-    }
-    else
-    {
-     	std::cout << "Данное r расширение не найдено" << std::endl << std::endl;
-        return bRes;
-    }
-}
-
-bool sVivod (std::string &vStr)
-{
-    bool bRes = false;
-    std::string sSubstr = vStr.substr(vStr.length() - 4);
-    std::cout << "Подстрока из последних 4 символов: " << "\""<< sSubstr << "\"" << std::endl;
-    if (sSubstr == ".xml")
-    {
-       std::cout << "Данная подстрока подходит" << std::endl << std::endl;
-       bRes = true;
-    }
-    else
-       std::cout << "Данная подстрока не подходит" << std::endl << std::endl;
     return bRes;
 }
 
@@ -124,25 +64,22 @@ int main(int argc, char* argv[])
     }
     std::cout << std::endl << std::endl;
     std::string sEnvir = getenv("NITAETC");
-    std::vector<std::string> vStr;
+    std::vector<std::string> vFileName;
     boost::filesystem::directory_iterator it(sEnvir), end;
     try
     {
         for ( ; it != end; ++it)
-            vStr.push_back(it->path().filename());
+            vFileName.push_back(it->path().filename());
     }
     catch (const std::exception& e)
     {
         std::cerr << "Exception: " << e.what();
         return 1;
     }
-    for (unsigned int i = 0; i < vStr.size(); i++)
+    for (unsigned int i = 0; i < vFileName.size(); i++)
     {
-        std::cout << vStr[i] << std::endl;
-        checkSystem (vStr[i]);
-        checkXml (vStr[i]);
-        checkRxml (vStr[i]);
-        sVivod (vStr[i]);
+        if (checkSystem(vFileName[i]) && checkXml(vFileName[i]))
+            std::cout << vFileName[i] << std::endl;
     }
     lib.Free();
     return 0;
